bf5bcb826169253ef94ff48cbb23cc3c0b646eb3
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-01-22 09:12:58 -0600

    fixed forms.py

--- placeholder/register/views.py
+++ placeholder/register/views.py
@@ -8,7 +8,7 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
+             form.save()
 
         return render(response, "register/register.html", {"form": form})
     else:
