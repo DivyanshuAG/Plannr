8ba79ea90668c00bb3299f3d7ed6084dd7a0b8c0
Johannes <jonseyplayz@gmail.com>
2022-01-22 22:16:46 +0100

    Replaced placeholder text with icons

--- placeholder/register/views.py
+++ placeholder/register/views.py
@@ -10,7 +10,7 @@ def register(response):
         if form.is_valid():
             form.save()
 
-        return render(response, "registration/register.html", {"form": form})
+        return redirect('main')
     else:
         form = RegisterForm()
 
