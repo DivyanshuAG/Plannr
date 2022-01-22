c19f334f15c061b03c1545cb490a6cfc2258017e
Divyanshu Agarwal <divyu.ag@gmail.com>
2022-01-22 23:52:30 +0530

    Days UI change
    Padded days and added day names on the top

--- placeholder/month/views.py
+++ placeholder/month/views.py
@@ -6,11 +6,12 @@ from week.models import Week
 from datetime import datetime
 
 # Create your views here.
+
+
 def monthView(request, name):
     if request.method == 'GET':
 
-        
-        this_month = name.lower().capitalize() # standardizes the URL
+        this_month = name.lower().capitalize()  # standardizes the URL
         month_object = Month.objects.get(name=this_month)
 
         # send necesary data to the template to be rendered
@@ -18,10 +19,13 @@ def monthView(request, name):
             'month': month_object,
             'dates': Day.objects.filter(month=month_object),
             'weeks': Week.objects.filter(month=month_object),
-            'currentYear': datetime.now().year
+            'currentYear': datetime.now().year,
+            'pad_range': range(0, month_object.starting_date+1),
+            'days_of_week':['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
         }
-    
+
         return render(request, template_name='month/index.html', context=data)
+
 
 def dayView(request, name, date):
     if request.method == 'GET':
@@ -33,5 +37,4 @@ def dayView(request, name, date):
             'specificDay': Day.objects.get(month=month_object, date=date),
             'month': month_object,
         }
-        return render(request,'month/index.html', context=data)
-        
+        return render(request, 'month/index.html', context=data)
