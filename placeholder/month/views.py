81832fa9e3a61f6d670180fd79980f15d96f4faf
Johannes <jonseyplayz@gmail.com>
2022-01-22 21:46:25 +0100

    CRAZY CALCULATIONS
    insane i had to write mental acrobatics calculation to make end-padding work

--- placeholder/month/views.py
+++ placeholder/month/views.py
@@ -27,6 +27,14 @@ def monthView(request, name):
 
         previous_month_object = Month.objects.get(name=previous_month)
         
+        def getRange():
+            remainder = month_object.amountOfDays % 7
+            #print(remainder)
+            if not remainder and month_object.starting_date == 0:
+                return 0
+            n = len(range(previous_month_object.amountOfDays - month_object.starting_date, previous_month_object.amountOfDays+1)) + month_object.amountOfDays
+            return range(1, 7 - (n % 7) +1)
+
         data = {
             # send necesary data to the template to be rendered
             'month': month_object,
@@ -36,8 +44,8 @@ def monthView(request, name):
             'pad_range': range(previous_month_object.amountOfDays - month_object.starting_date, previous_month_object.amountOfDays+1),
             'days_of_week':['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
             'prev_month': previous_month,
-            'next_month': next_month
-
+            'next_month': next_month,
+            'over_pad_range': getRange()
         }
 
         return render(request, template_name='month/index.html', context=data)
