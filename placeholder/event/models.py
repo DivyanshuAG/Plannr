d5266529c3142c3c68d01b96c274b81d6356ca5e
Johannes <jonseyplayz@gmail.com>
2022-01-22 11:13:57 +0100

    made template view

--- placeholder/event/models.py
+++ placeholder/event/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from day.models import Day 
 
 class Event(models.Model): 
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    dates_affected = models.ManyToManyField(Day)
+    
+    def __init__(self):
+        
--- placeholder/month/views.py
+++ placeholder/month/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import Month
+from day.models import Day
 
 # Create your views here.
 def monthView(request, name):
     if request.method == 'GET':
-
+        this_month= name.lower().capitalize()
         print(name)
+        month_object = Month.objects.get(name=this_month)
 
         data = {
-            'month': Month.objects.get(name=name.lower().capitalize())
+            'month': month_object,
+            'dates': Day.objects.filter(month=month_object)
         }
     
         return render(request, template_name='month/index.html', context=data)
